05b0dd2e712706d2c6642b1f151d9f71421f47ca
Nancy McBride <njmcbride224@gmail.com>
2021-05-04 01:23:39 -0500

    adding files for navbar functionalty

--- nancymcbrideportfolio/src/App.js
+++ nancymcbrideportfolio/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Aboutme from './components/pages/aboutme';
+import Projects from '.components/pages/projects';
+import Contactme from '.components/pages/contactme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<Router>
+	<Navbar />
+	<Switch>
+		<Route path='./pages/aboutme' component={Aboutme} />
+		<Route path='./pages/projects' component={Projects}/>
+		<Route path='./pages/contactme' component={Contactme} />
+	</Switch>
+	</Router>
+);
 }
 
 export default App;
